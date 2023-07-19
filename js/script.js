d2df01cf7b1defff4e8aa958cdd78029b88acd38
RodiaGiordano <rodiagiordano93@gmail.com>
2023-07-19 16:37:30 +0200

    feat: feat and fix javascript

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+const userAge = parseInt(prompt ("inserisci la tua età"));
+
+const userKm = parseFloat(prompt ("inserisci il nuemro chilometri che vuoi percorrere"));
+
+if (isNaN (userAge) || isNaN(userKm)){
+    alert("Accetta solo caratteri numerici - premere F5")
+} 
+
+else {
+
+    
+    const price = 0.21;
+
+    const discount20 = (price * userKm) * 0.8;
+    
+    const discount40 = (price * userKm) * 0.6;
+
+
+if (userAge >65){
+
+    ticketPrice = discount40;       
+}
+
+else if (userAge <18){
+    ticketPrice = discount20;
+}
+
+else {
+    ticketPrice = price * userKm;
+}
+
+
+alert("il prezzo del biglietto è: " + ticketPrice.toFixed(2) + " €");
+
+
+}
+
+
+
